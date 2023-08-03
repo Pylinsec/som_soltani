6c80eee6541a8be5b052aa94553be66c3cc5947a
abolbdh <abol.binandeh@gmail.com>
2023-08-03 19:49:09 +0330

    sendig 12

---
+++ session 12/list_v1.py
@@ -0,0 +1,7 @@
+# nested loop
+for k in range(3):
+    for j in range(3):
+        for i in range(3):
+            print(i,end=' ')
+        print() 
+    print()      
---
+++ session 12/while1.py
@@ -0,0 +1,12 @@
+# i = 0
+# while i< 10:
+#     print(i , end=' ')
+#     i += 1
+
+# for i in range(10):
+#     print(i)    
+
+for som in ('soltani'):
+    print(som,end=' ')  
+    print(som.upper(),end=' ') 
+    print(som.title())
